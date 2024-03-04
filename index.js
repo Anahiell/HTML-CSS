a61f44e5db97ce23509da59e88f7de2b62e1d10e
Anahaiell <anahielit@gmail.com>
2024-03-04 12:32:15 +0200

    js homework

--- index.js
+++ index.js
@@ -1,90 +1,66 @@
-function power(base, exponent) {
-    if (exponent === 0) {
-        return 1;
-    } else if (exponent === 1) {
-        return base;
-    } else {
-        return base * power(base, exponent - 1);
-    }
-}
-function gcd(a, b) {
-    if (b === 0) {
-        return a;
-    } else {
-        return gcd(b, a % b);
-    }
-}
-function maxDigit(number) {
-    if (number < 10) {
-        return number;
-    } else {
-        let lastDigit = number % 10;
-        let remainingDigits = Math.floor(number / 10);
-        let maxInRest = maxDigit(remainingDigits);
-        return Math.max(lastDigit, maxInRest);
-    }
-}
-function isPrime(number, divisor = 2) {
-    if (number <= 2) {
-        return (number === 2);
-    }
-    if (number % divisor === 0) {
-        return false;
-    }
-    if (divisor * divisor > number) {
-        return true;
-    }
-    return isPrime(number, divisor + 1);
-}
-function factors(number, divisor = 2) {
-    if (number < 2) {
-        return [];
-    }
-    if (number % divisor === 0) {
-        return [divisor, ...factors(number / divisor, divisor)];
-    }
-    return factors(number, divisor + 1);
-}
-function fibonacci(n) {
-    if (n <= 2) {
-        return 1;
-    }
-    return fibonacci(n - 1) + fibonacci(n - 2);
-}
-function calculatePower() {
-    const base = document.getElementById("base").value;
-    const exponent = document.getElementById("exponent").value;
-    const result = power(parseInt(base), parseInt(exponent));
-    document.getElementById("powerResult").innerText = result;
+document.addEventListener('DOMContentLoaded', function() {
+    const usernameInput = document.getElementById('usernameInput');
+
+    usernameInput.addEventListener('input', function(event) {
+        const inputText = event.target.value;
+        let filteredText = '';
+
+        for (let i = 0; i < inputText.length; i++) {
+            const char = inputText[i];
+            if (!isNaN(char)) { // Проверка, является ли символ цифрой
+                continue; // Если цифра, пропускаем её
+            }
+            filteredText += char; // Добавляем ненумерический символ к отфильтрованному тексту
+        }
+
+        event.target.value = filteredText; // Обновляем 
+    });
+});
+
+
+// Получаем ссылки на кнопку и модальное окно
+var openModalBtn = document.getElementById('openModalBtn');
+var modal = document.getElementById('myModal');
+var closeBtn = document.getElementsByClassName('close')[0];
+
+openModalBtn.onclick = function() {
+  modal.style.display = 'block';
 }
 
-function calculateGCD() {
-    const number1 = document.getElementById("number1").value;
-    const number2 = document.getElementById("number2").value;
-    const result = gcd(parseInt(number1), parseInt(number2));
-    document.getElementById("gcdResult").innerText = result;
+closeBtn.onclick = function() {
+  modal.style.display = 'none';
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
 }
 
-function findMaxDigit() {
-    const number = document.getElementById("number").value;
-    const result = maxDigit(parseInt(number));
-    document.getElementById("maxDigitResult").innerText = result;
-}
 
-function checkPrime() {
-    const number = document.getElementById("primeNumber").value;
-    const result = isPrime(parseInt(number));
-    document.getElementById("primeResult").innerText = result;
-}
+//lighter
+var lights = document.querySelectorAll('.light');
+var toggleBtn = document.getElementById('toggleBtn');
+var activeIndex = 0;
+function toggleLights() {
+    for (var i = 0; i < lights.length; i++) {
+      lights[i].classList.add('grey');
+    }
 
-function findFactors() {
-    const number = document.getElementById("factorNumber").value;
-    const result = factors(parseInt(number));
-    document.getElementById("factorResult").innerText = result.join(", ");
-}
+    lights[activeIndex].classList.remove('grey');
+    activeIndex = (activeIndex + 1) % lights.length;
+  }
+toggleBtn.addEventListener('click', toggleLights);
 
-function calculateFibonacci() {
-    const number = document.getElementById("fibonacciNumber").value;
-    const result = fibonacci(parseInt(number));
-    document.getElementById("fibonacciResult").innerText = result;
-}
+var books = document.querySelectorAll('.book');
+
+  // Додаємо обробник подій для кожної книги
+  books.forEach(function(book) {
+    book.addEventListener('click', function() {
+      // Видаляємо клас 'selected' з усіх книг
+      books.forEach(function(book) {
+        book.classList.remove('selected');
+      });
+      // Додаємо клас 'selected' до поточної книги
+      book.classList.add('selected');
+    });
+  });
